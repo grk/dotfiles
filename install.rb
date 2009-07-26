3cc7b7dd6211a5da572f1c9e9818a0db0ac53f3a
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2009-07-26 22:31:01 +0200

    changes

--- install.rb
+++ install.rb
@@ -7,6 +7,8 @@ home = File.expand_path('~')
 Dir['*'].each do |file|
   next if file =~ /install/
   target = File.join(home, ".#{file}")
-  `ln -s #{File.expand_path file} #{target}`
+  unless File.exist? target
+    `ln -vsf #{File.expand_path file} #{target}`
+  end
 end
 
